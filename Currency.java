bdf574bb871446c299de812463d8ce5323bafe6f
nunoboleto <n_boleto@hotmail.com>
2015-03-04 20:40:21 +0000

    Aula 2 - Exercício 1

--- Currency.java
+++ Currency.java
@@ -9,16 +9,16 @@ Class Currency{
 		this.sym = sym;
 		this.value = value;
 	}
-	
+
 	public String toString(){
 
 		String msg = "";
-		msg = (sym, value);
+		msg = (sym + value);
 		return(msg);
 	}
 
 	public static void main (String[] arg){
 		Currency c1 = new Currency("€", 5);
-		System.out.println(c1.toString());	
+		System.out.println(c1.toString());
 	}
 }
